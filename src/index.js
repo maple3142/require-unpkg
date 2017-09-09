f893e82165890b0debfe068c360dec88c4500038
maple3142 <kirito_@mail.edu.tw>
2017-09-09 11:40:14 +0800

    0.0.1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
-export default {}
+import axios from 'axios'
+
+//module cache
+const cache={}
+
+//internal require
+async function _require(pkg) {
+	if (!(pkg in cache)) {
+		let exports = {}
+		let module = { exports }
+		let d = await axios.get(`https://unpkg.com/${pkg}`)
+		eval(d.data)
+		cache[pkg]=module
+	}
+	return cache[pkg].exports
+}
+
+/**
+ * require module from unpkg.com
+ * @param {String|String[]} package module name or modules name
+ * @return {Promise<Object>|Promise<Object[]>} return promise with module or array of module
+ */
+async function require(pkg) {
+	if (!Array.isArray(pkg))
+		return _require(pkg)
+	else {
+		let pms = pkg.map(p => _require(p))
+		return await Promise.all(pms)
+	}
+}
+
+export default require
+if (typeof window !== 'undefined') {
+	window.require=require
+}
